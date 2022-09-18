2b3797d7eb097947f61d9182d261f60320c643bf
Amstf <ali_mustapha_@outlook.com>
2022-09-18 20:48:32 +0200

    upload the aboutUs.php

---
+++ AboutUs.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>About Us</title>
+    <link rel="stylesheet" href="form.css" />
+    <link rel="stylesheet" href="HeaderStyle.css" />
+    <link rel="icon" href="./Images/b.jpeg" />
+    <a name="top"></a>
+    <!-- fontawsone -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
+      integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+<?php include 'header.php' ;?>
+    <div style="background-color: #292929">
+      <iframe
+        src="Slider.html"
+        name="iframe"
+        width="100%"
+        height="600px"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+
+      <iframe
+        style="margin-top: 50px"
+        src="AboutUsBoot.html"
+        name="iframe"
+        width="100%"
+        height="1600px"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+
+    <?php include 'footer.html'; ?>
+
+  </body>
+</html>
